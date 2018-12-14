dec75a98ac18b8547725a676a1d03baa803fbff3
Hang Cui <oscarcuihang@hotmail.com>
2018-12-14 17:07:56 -0500

    Create Blog MVC, allow modify blogs (#2)

--- Gemfile
+++ Gemfile
@@ -30,6 +30,9 @@ gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
+  gem 'pry-byebug'
+  gem 'faker'
 end
 
 group :development do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_213501) do
+ActiveRecord::Schema.define(version: 2018_12_13_181956) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.text "content"
+    t.boolean "published", default: false, null: false
+    t.integer "view_count", default: 0, null: false
+    t.datetime "published_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
