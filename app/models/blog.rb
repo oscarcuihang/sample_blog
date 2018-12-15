6de5039cae3ec276fa0dcaaa5fc73e6c8344d672
Hang Cui <oscarcuihang@hotmail.com>
2018-12-15 13:34:29 -0500

    Add Search Blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,12 @@
 class Blog < ApplicationRecord
   belongs_to :user
   scope :published, -> { where(published: true) }
+
+  def self.search(term)
+    if term
+      published.where('title LIKE ?', "%#{term}%")
+    else
+      published.all
+    end
+  end
 end
